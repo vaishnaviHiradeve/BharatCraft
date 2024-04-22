586740411c0666d9b78ca22f4ab79d26796d2ee0
Vaishnavi <vaishnavihira31@gmail.com>
2024-04-22 17:26:22 +0530

    first commit

---
+++ Data/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using BharatCrafted.Data;
+using BharatCrafted.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ShoppingCart.Infrastructure
+{
+    public class SeedData
+    {
+        public static void SeedDatabase(ApplicationDbContext context)
+        {
+            context.Database.Migrate();
+
+            if (!context.prods.Any())
+            {
+                Category Metallic = new Category { Name = "Metallic", Slug = "Metallic" };
+                Category Wooden = new Category { Name = "Wooden", Slug = "Wooden" };
+
+                context.prods.AddRange(
+                        new Prod
+                        {
+                            Name = "Dancing lady",
+                            Slug = "Dancing lady",
+                            Description = "Metallic dancing lady",
+                            Price = 12.99M,
+                            Category = Metallic,
+                            Image = "shi.png"
+                        },
+                        new Prod
+                        {
+                            Name = "Saur",
+                            Slug = "Saur",
+                            Description = "Classical saur",
+                            Price = 12.99M,
+                            Category = Wooden,
+                            Image = "saur.png"
+                        },
+                        new Prod
+                        {
+                            Name = "Shi",
+                            Slug = "Shi",
+                            Description = "Ancient Shi",
+                            Price = 12.99M,
+                            Category = Metallic,
+                            Image = "shi.png"
+                        },
+                        new Prod
+                        {
+                            Name = "Dancing lady",
+                            Slug = "Dancing lady",
+                            Description = "Metallic dancing lady",
+                            Price = 12.99M,
+                            Category = Metallic,
+                            Image = "saur.png"
+                        }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BharatCrafted.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Prod> Products { get; set; }
+    }
+}
